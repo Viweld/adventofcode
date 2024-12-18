12eabb047fde04ca22dc1505b913c49f3cf7dcc2
Ziborov Vadim <vadim.ziborov1985@gmail.com>
2024-12-18 19:55:38 +0500

    day one

---
+++ main.dart
@@ -0,0 +1,8 @@
+import 'puzzles/day_01.dart';
+
+void main() {
+  final resultOfPartOneDay1 = HistorianHysteria().solvePuzzlePartOne();
+  print('Day1: answer of part one: $resultOfPartOneDay1');
+  final resultOfPartTwoDay1 = HistorianHysteria().solvePuzzlePartTwo();
+  print('Day1: answer of part two: $resultOfPartTwoDay1');
+}
