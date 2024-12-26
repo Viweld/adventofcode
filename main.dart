2a4000c21dd912abb51f451ae5b0644f5d422274
Ziborov Vadim <vadim.ziborov1985@gmail.com>
2024-12-26 20:30:13 +0500

    day six

--- main.dart
+++ main.dart
@@ -4,14 +4,16 @@ import 'puzzles/day_02.dart';
 import 'puzzles/day_03.dart';
 import 'puzzles/day_04.dart';
 import 'puzzles/day_05.dart';
+import 'puzzles/day_06.dart';
 
 void main() {
-  final day1 = HistorianHysteria();
-  final day2 = RedNosedReports();
-  final day3 = MullItOver();
-  final day4 = CeresSearch();
-  final day5 = PrintQueue();
-  solve(day5);
+  // final day1 = HistorianHysteria();
+  // final day2 = RedNosedReports();
+  // final day3 = MullItOver();
+  // final day4 = CeresSearch();
+  // final day5 = PrintQueue();
+  final day6 = GuardGallivant();
+  solve(day6);
 }
 
 void solve(DailyChallenge challenge) {
